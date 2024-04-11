3957d63acd4dd1863bfe92df813d160377c5b093
davidfer1112 <davidfernando1112@gmail.com>
2024-04-10 22:16:54 -0500

    Feature: pingu cone

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h1> Holaaa 🐧🐇</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
